f261b94a79ba9b006fdf955171369c1e0053c03f
aed3 <32887801+aed3@users.noreply.github.com>
2018-05-22 17:27:05 -0500

    Angles and more threads

--- Object Tracking/main.cpp
+++ Object Tracking/main.cpp
@@ -35,6 +35,76 @@ const int MAX_OBJECT_AREA = FRAME_HEIGHT*FRAME_WIDTH/1.5;
 const Mat erodeElement = getStructuringElement(MORPH_RECT,Size(6,6));
 const Mat dilateElement = getStructuringElement(MORPH_RECT,Size(16,16));
 
+// Declaring Time Variables
+double ti = 0;
+double t2 = 0;
+double td = 0;
+double tf = 0;
+
+//Declaring Velocity and Gravity
+double g = 9.80665;
+double zv = 0;
+double xv = 0;
+double yv = 0;
+
+// Declaring Z Variables
+double zi = 0;
+double z2 = 0;
+double z3 = 0;
+double zf = 0;
+
+// Declaring X Variables
+double xi = 0;
+double x2 = 0;
+double x3 = 0;
+double xf = 0;
+
+// Declaring Y Variables
+double yi = 0;
+double y2 = 0;
+double y3 = 0;
+double yf = 0;
+
+// Declaring distance from center joint to position (hypotenuse) and angles
+double hyp = 0;
+double angle1 = 0;
+double angle2 = 0;
+
+// Declaring image holders
+Mat cameraFeedL, cameraFeedR;
+Mat HSVL, HSVR;
+Mat thresholdL, thresholdR;
+
+Mat ballCoordL(1,1,CV_64FC2);
+Mat ballCoordR(1,1,CV_64FC2);
+Mat undistoredCoordL;
+Mat undistoredCoordR;
+
+Mat P(4,1,CV_64F);
+
+// Distances in physical space (m)
+double dxCameras = 0.362;
+double dyGround = 1;
+double innerArmLength = .5;
+double outerArmLength = .5;
+double maxArmLength;
+double minArmLength;
+
+Mat KL(3,3,CV_64F, Scalar(0.0));
+Mat KR(3,3,CV_64F, Scalar(0.0));
+
+Mat distortion_coeffsL(5,1,CV_64F);
+Mat distortion_coeffsR(5,1,CV_64F);
+
+Mat transformL(3,4,CV_64F, Scalar(0.0));
+Mat transformR(3,4,CV_64F, Scalar(0.0));
+
+int amountFound = 0;
+bool objectFoundL = false;
+bool objectFoundR = false;
+bool showCrossHairs = true;
+bool showWindows = false;
+
 // Names that will appear at the top of each window
 const string windowNameL = "Left Original Image";
 const string windowNameL1 = "Left HSV Image";
@@ -184,6 +254,45 @@ void trackFilteredObject(double &x, double &y, Mat threshold, Mat &cameraFeed, b
 	}
 }
 
+/*
+* Need to factor in ball bounce
+*   Set so if ball's yf is below or at the ground (defined by the distance between the cameras and ground), have it make yf = groundY - (yf - groundY)
+* Need to do parabolic trajectory to account for curve shots
+*   If we have three points, use the following to find C in u=Av^2+Bv+C
+        D = (v1 - v2)(v1 - v3)(v2 - v3)
+        C = (v2 * v3 * (v2 - v3) * u1 + v3 * v1 * (v3 - v1) * u2 + v1 * v2 * (v1 - v2) * u3) / D
+*   Do once with u=x,v=z and another with u=y,v=z and set the value of C to xf and yf since the ball reaches the robot at z=0
+*/
+void findFinalAndAngles() {
+    if (amountFound == 2) {
+        td = t2 - ti;
+
+        zv = (z2 - zi)/td;
+        xv = (x2 - xi)/td;
+        yv = (y2 - yi)/td;
+
+        tf = (zf - zi)/zv + ti;
+
+        xf = xv * tf;
+        yf = 0.5 * g * (tf*tf) + yv * tf + yi;
+    }
+    else if (amountFound > 2) {
+        xf = (z2 * z3 * (z2 - z3) * xi + z3 * zi * (z3 - zi) * x2 + zi * z2 * (zi - z2) * x3) / ((zi - z2) * (zi - z3) * (z2 - z3));
+        yf = (z2 * z3 * (z2 - z3) * yi + z3 * zi * (z3 - zi) * y2 + zi * z2 * (zi - z2) * y3) / ((zi - z2) * (zi - z3) * (z2 - z3));
+    }
+
+    if (yf < -dyGround) {
+        yf = -dyGround - (yf + dyGround);
+    }
+
+    hyp = sqrt(xf*xf + yf*yf);
+    if (hyp > maxArmLength) hyp = maxArmLength;
+    if (hyp < minArmLength) hyp = minArmLength;
+
+    angle2 = acos((hyp*hyp - innerArmLength*innerArmLength - outerArmLength*outerArmLength)/(2*innerArmLength*outerArmLength));
+    angle1 = atan2(yf,xf) - atan((outerArmLength*sin(angle2))/(innerArmLength+outerArmLength*cos(angle2)));
+}
+
 void GetCooridnates(VideoCapture capture, double* x, double* y, Mat* cameraFeed, Mat* HSV, Mat* threshold, bool* objectFound, bool showCrossHairs, bool LeftCam) {
 	vector<Vec3f> circles;
 
@@ -191,7 +300,7 @@ void GetCooridnates(VideoCapture capture, double* x, double* y, Mat* cameraFeed,
     long long start = PerformanceCounter();
     capture.read(*cameraFeed);
     if (LeftCam) readt += timeOfFunction(start, "read");
-    if (LeftCam) *cameraFeed = *cameraFeed*5;
+    //if (LeftCam) *cameraFeed = *cameraFeed*1;
 
     // Converts frame from BGR to HSV colorspace
     if (LeftCam) start = PerformanceCounter();
@@ -222,88 +331,80 @@ void GetCooridnates(VideoCapture capture, double* x, double* y, Mat* cameraFeed,
     if (LeftCam) findCirlcest += timeOfFunction(start, "findCircles");
 }
 
+void findRealandPosition(double xcL, double ycL, double xcR, double ycR, Mat projL, Mat projR) {
+    long long start = PerformanceCounter();
+    if (objectFoundL && objectFoundR) {
+        amountFound++;
+
+        ballCoordL.at<Vec2d>(0,0)[0] = xcL;
+        ballCoordL.at<Vec2d>(0,0)[1] = ycL;
+        ballCoordR.at<Vec2d>(0,0)[0] = xcR;
+        ballCoordR.at<Vec2d>(0,0)[1] = ycR;
+
+        undistortPoints(ballCoordL,undistoredCoordL,KL,distortion_coeffsL);
+        undistortPoints(ballCoordR,undistoredCoordR,KR,distortion_coeffsR);
+
+        triangulatePoints(projL, projR, ballCoordL, ballCoordR, P);
+
+        x3 = x2; y3 = y2; z3 = z2;
+        x2 = xi; y2 = yi; z2 = zi; t2 = ti;
+
+        xi = -P.at<double>(0,0)/P.at<double>(3,0) + dxCameras/2;//triangulate points outputs;
+        yi = -P.at<double>(1,0)/P.at<double>(3,0);//triangulate points outputs;
+        zi = -P.at<double>(2,0)/P.at<double>(3,0);//triangulate points outputs;
+        ti = (PerformanceCounter() / PerformanceFrequency()) * 1000000.0;
+
+        findFinalAndAngles();
+    }
+    else {
+        amountFound = 0;
+    }
+    calcPositiont += timeOfFunction(start, "calcPosition");
+}
+
 int main(int argc, char* argv[]) {
 
+    // Old camera = RIGHT
 	double xcL, ycL = 0;
 	double xcR, ycR = 0;
 
-	// Declaring Time Variables
-	double ti = 0;
-	double t2 = 0;
-	double td = 0;
-	double tf = 0;
+    KL.at<double>(0,0) = 1072.596243797988791;
+    KL.at<double>(1,1) = 1073.690381426655222;
+    KL.at<double>(0,2) = 680.723893064209733;
+    KL.at<double>(1,2) = 383.222991477372091;
+    KL.at<double>(2,2) = 1;
 
-	//Declaring Velocity and Gravity
-	double g = 9.80665;
-	double zv = 0;
-	double xv = 0;
-	double yv = 0;
+    KR.at<double>(0,0) = 1051.570474935752145;
+    KR.at<double>(1,1) = 1052.831434088737069;
+    KR.at<double>(0,2) = 630.837501734280636;
+    KR.at<double>(1,2) = 341.993415934465418;
+    KR.at<double>(2,2) = 1;
 
-	// Declaring Z Variables
-	double zi = 0;
-	double z2 = 0;
-	double z3 = 0;
-	double zf = 0;
+    distortion_coeffsL.at<double>(0,0) = -0.407242377983198;
+    distortion_coeffsL.at<double>(1,0) = 0.163506544703868;
+    distortion_coeffsL.at<double>(2,0) = -0.000309830974174;
+    distortion_coeffsL.at<double>(3,0) = 0.001234732556889;
+    distortion_coeffsL.at<double>(4,0) = 0.000000000000000;
 
-	// Declaring X Variables
-	double xi = 0;
-	double x2 = 0;
-	double x3 = 0;
-	double xf = 0;
+    distortion_coeffsR.at<double>(0,0) = -0.395033423474349;
+    distortion_coeffsR.at<double>(1,0) = 0.157550161003169;
+    distortion_coeffsR.at<double>(2,0) = 0.000064885496368;
+    distortion_coeffsR.at<double>(3,0) = 0.000488116203253;
+    distortion_coeffsR.at<double>(4,0) = 0.000000000000000;
 
-	// Declaring Y Variables
-	double yi = 0;
-	double y2 = 0;
-	double y3 = 0;
-	double yf = 0;
-
-    // Declaring image holders
-	Mat cameraFeedL, cameraFeedR;
-	Mat HSVL, HSVR;
-	Mat thresholdL, thresholdR;
-
-    Mat ballCoordL(1,1,CV_64FC2);
-    Mat ballCoordR(1,1,CV_64FC2);
-    Mat undistoredCoordL;
-    Mat undistoredCoordR;
-
-    Mat P(4,1,CV_64F);
-
-    // Distance between each camera and the cameras and the ground
-    double dxCameras = 0.362;
-    double dyGround = 1;
-
-    Mat K(3,3,CV_64F, Scalar(0.0));
-    K.at<double>(0,0) = 995.017352724241164;
-    K.at<double>(1,1) = 996.788504391169340;
-    K.at<double>(0,2) = 639.754371102307573;
-    K.at<double>(1,2) = 371.061867974806205;
-    K.at<double>(2,2) = 1;
-
-    Mat distortion_coeffs(5,1,CV_64F);
-    distortion_coeffs.at<double>(0,0) = -0.392080477762577;
-    distortion_coeffs.at<double>(1,0) = 0.144522160514508;
-    distortion_coeffs.at<double>(2,0) = -0.003338987421132;
-    distortion_coeffs.at<double>(3,0) = -0.000291551741632;
-    distortion_coeffs.at<double>(4,0) = 0.000000000000000;
-
-    Mat transformL(3,4,CV_64F, Scalar(0.0));
-    Mat transformR(3,4,CV_64F, Scalar(0.0));
     transformL.at<double>(0,0) = 1.0; transformR.at<double>(0,0) = 1.0;
     transformL.at<double>(1,1) = 1.0; transformR.at<double>(1,1) = 1.0;
     transformL.at<double>(2,2) = 1.0; transformR.at<double>(2,2) = 1.0;
     transformL.at<double>(0,3) = -dxCameras/2; transformR.at<double>(0,3) = dxCameras/2;
 
-    Mat projL = K*transformL;
-    Mat projR = K*transformR;
+    Mat projL = KL*transformL;
+    Mat projR = KR*transformR;
 
-	bool objectFoundL = false;
-	bool objectFoundR = false;
-	bool showCrossHairs = true;
-	bool showWindows = false;
+    maxArmLength = innerArmLength + outerArmLength;
+    minArmLength = innerArmLength - outerArmLength;
 
-	VideoCapture captureL(0);
-	VideoCapture captureR(0);
+	VideoCapture captureL(2);
+	VideoCapture captureR(1);
 
     captureL.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'));
 	captureL.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);
@@ -315,7 +416,6 @@ int main(int argc, char* argv[]) {
 
     time_t timeBegin = time(0);
     int tick = 0;
-    int amountFound = 0;
 
 	createTrackbars();
     namedWindow(windowNameL, WINDOW_NORMAL);
@@ -328,72 +428,11 @@ int main(int argc, char* argv[]) {
 	while(1) {
         thread cameraL(GetCooridnates, captureL, &xcL, &ycL, &cameraFeedL, &HSVL, &thresholdL, &objectFoundL, showCrossHairs, true);
         thread cameraR(GetCooridnates, captureR, &xcR, &ycR, &cameraFeedR, &HSVR, &thresholdR, &objectFoundR, showCrossHairs, false);
+        thread angles(findRealandPosition, xcL, ycL, xcR, ycR, projL, projR);
 
         cameraL.join();
         cameraR.join();
-
-        /*if (objectFoundL)
-            cout << "X1: " << xc1 << " / Y1: " << yc1 << endl;
-        if (objectFoundR)
-            cout << "X2: " << xc2 << " / Y2: " << yc2 << endl;
-            */
-        long long start = PerformanceCounter();
-        if (objectFoundL && objectFoundR) {
-            amountFound++;
-
-            ballCoordL.at<Vec2d>(0,0)[0] = xcL;
-            ballCoordL.at<Vec2d>(0,0)[1] = ycL;
-            ballCoordR.at<Vec2d>(0,0)[0] = xcR;
-            ballCoordR.at<Vec2d>(0,0)[1] = ycR;
-
-            undistortPoints(ballCoordL,undistoredCoordL,K,distortion_coeffs);
-            undistortPoints(ballCoordR,undistoredCoordR,K,distortion_coeffs);
-
-            triangulatePoints(projL, projR, ballCoordL, ballCoordR, P);
-
-            x3 = x2; y3 = y2; z3 = z2;
-            x2 = xi; y2 = yi; z2 = zi; t2 = ti;
-
-            xi = -P.at<double>(0,0)/P.at<double>(3,0) + dxCameras/2;//triangulate points outputs;
-            yi = -P.at<double>(1,0)/P.at<double>(3,0);//triangulate points outputs;
-            zi = -P.at<double>(2,0)/P.at<double>(3,0);//triangulate points outputs;
-            ti = (PerformanceCounter() / PerformanceFrequency()) * 1000000.0;
-
-            /*
-            * Need to factor in ball bounce
-            *   Set so if ball's yf is below or at the ground (defined by the distance between the cameras and ground), have it make yf = groundY - (yf - groundY)
-            * Need to do parabolic trajectory to account for curve shots
-            *   If we have three points, use the following to find C in u=Av^2+Bv+C
-                    D = (v1 - v2)(v1 - v3)(v2 - v3)
-                    C = (v2 * v3 * (v2 - v3) * u1 + v3 * v1 * (v3 - v1) * u2 + v1 * v2 * (v1 - v2) * u3) / D
-            *   Do once with u=x,v=z and another with u=y,v=z and set the value of C to xf and yf since the ball reaches the robot at z=0
-            */
-
-            if (amountFound == 2) {
-                td = t2 - ti;
-
-                zv = (z2 - zi)/td;
-                xv = (x2 - xi)/td;
-                yv = (y2 - yi)/td;
-
-                tf = (zf - zi)/zv + ti;
-
-                xf = xv * tf;
-                yf = 0.5 * g * (tf*tf) + yv * tf + yi;
-            }
-            else if (amountFound > 2) {
-                xf = (z2 * z3 * (z2 - z3) * xi + z3 * zi * (z3 - zi) * x2 + zi * z2 * (zi - z2) * x3) / ((zi - z2) * (zi - z3) * (z2 - z3));
-                yf = (z2 * z3 * (z2 - z3) * yi + z3 * zi * (z3 - zi) * y2 + zi * z2 * (zi - z2) * y3) / ((zi - z2) * (zi - z3) * (z2 - z3));
-            }
-
-            if (yf < -dyGround) {
-                yf = -dyGround - (yf + dyGround);
-            }
-        }
-        else {
-            amountFound = 0;
-        }
-        calcPositiont += timeOfFunction(start, "calcPosition");
+        angles.join();
 
         // Extra stuff used for testing time and showing windows
 
@@ -411,7 +450,8 @@ int main(int argc, char* argv[]) {
             cout << "findCirlces: " << findCirlcest / frameCounter << endl;
             cout << "calcPosition: " << calcPositiont / frameCounter << endl;
             cout << P/P.at<double>(3,0) << endl;
-            frameCounter = 0; readt = 0; cvtColort = 0; inRanget = 0; morphOpst = 0; trackFilteredObjectt = 0, findCirlcest = 0;
+            cout << "xf: " << xf << ", yf: " << yf << endl;
+            frameCounter = 0; readt = 0; cvtColort = 0; inRanget = 0; morphOpst = 0; trackFilteredObjectt = 0, findCirlcest = 0, calcPositiont = 0;
         }
 
         if (GetAsyncKeyState(VK_CAPITAL))
