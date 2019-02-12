55e11ceec3c17365eb30468a670228981abb90bc
Teephop Keerasuntonpong <floatkeera@gmail.com>
2019-02-11 21:15:02 -0600

    first commit

---
+++ Object Tracking/build/CMakeFiles/cv_nurc_lax.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_nurc_lax.dir/main.cpp.o"
+  "cv_nurc_lax.pdb"
+  "cv_nurc_lax"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_nurc_lax.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
