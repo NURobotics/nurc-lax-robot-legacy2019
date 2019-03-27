7f830cd0a7f823e8da2203df74dcf391888bdd66
aed3 <32887801+aed3@users.noreply.github.com>
2019-03-27 17:10:06 -0500

    Update main.cpp

--- Object Tracking/main.cpp
+++ Object Tracking/main.cpp
@@ -1,10 +1,8 @@
 #include <string>
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/videoio/videoio.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/core/cuda.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 #include "LGR/axis.hpp"
 #include "LGR/HSV.hpp"
