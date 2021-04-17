6f95dfa6ff14b4771722f1686f9cb7f26d6d2fb9
Medet <medetkanmamyrov2022@u.northwestern.edu>
2021-04-17 15:23:21 -0500

    init Socket

---
+++ MainEvent/Server.cpp
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+// TODO: use arpa/inet.h on Mac and netinet/in.h on Linux/Jetson
+// could add if to check for OS
+//#include <netinet/in.h>
+#include <arpa/inet.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+// TODO: have a predefined port number instead of passing through argc
+int main(int argc, char *argv[])
+{
+    // sockfd is listening for different connections
+    // newsockfd is for handling currect connection
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    // buffer is used to read messages coming from client
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    // create a socket
+    // socket(int domain, int type, int protocol)
+    sockfd =  socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    // clear address structure. Prepping serv address
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    portno = atoi(argv[1]);
+
+    /* setup the host_addr structure for use in bind call */
+    // server byte order
+    serv_addr.sin_family = AF_INET;
+
+    // automatically be filled with current host's IP address
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // convert short integer value for port must be converted into network byte order
+    serv_addr.sin_port = htons(portno);
+
+    // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
+    // bind() passes file descriptor, the address structure,
+    // and the length of the address structure
+    // This bind() call will bind  the socket to the current IP address on port, portno
+    if (bind(sockfd,
+                (struct sockaddr *) &serv_addr,
+                sizeof(serv_addr))
+            < 0)
+        error("ERROR on binding");
+
+    // This listen() call tells the socket to listen to the incoming connections.
+    // The listen() function places all incoming connection into a backlog queue
+    // until accept() call accepts the connection.
+    // Here, we set the maximum size for the backlog queue to 5.
+    listen(sockfd,5); // TODO: look at the necessary queue size. What happens if queue is full?
+
+    // The accept() call actually accepts an incoming connection
+    clilen = sizeof(cli_addr);
+
+    // This accept() function will write the connecting client's address info
+    // into the the address structure and the size of that structure is clilen.
+    // The accept() returns a new socket file descriptor for the accepted connection.
+    // So, the original socket file descriptor can continue to be used
+    // for accepting new connections while the new socker file descriptor is used for
+    // communicating with the connected client.
+    newsockfd = accept(sockfd,
+            (struct sockaddr *) &cli_addr,
+            &clilen
+            );
+    if (newsockfd < 0)
+        error("ERROR on accept");
+
+    printf("server: got connection from %s port %d\n",
+            inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+
+
+    // This send() function sends the 13 bytes of the string to the new socket
+    send(newsockfd, "Hello, world!\n", 13, 0);
+
+    bzero(buffer, 256);
+
+    n = read(newsockfd, buffer, 255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n",buffer);
+
+    // TODO: read the buffer information. loop if incoming message is larger
+    // than the buffer size
+
+    // TODO: close client socket connection only when explicitly told or the web
+    // app is closed
+    close(newsockfd);
+    // TODO: close listening fd when closing/turning off the server
+    close(sockfd);
+    return 0;
+};
